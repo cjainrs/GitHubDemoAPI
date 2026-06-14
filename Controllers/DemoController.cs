63126da224eecf7ee4a00924c53d4f444eec5d49
Chetan Jain <cjainrs@gmail.com>
2026-06-14 20:34:56 +0530

    DemoController

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitHubDemoAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Git Stash Demo");
+        }
+    }
+}
+    
+
